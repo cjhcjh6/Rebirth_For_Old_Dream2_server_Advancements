e9357de3263c7a0961922c450fac4625d01f8dec
cjhcjh6 <141553054+cjhcjh6@users.noreply.github.com>
2024-03-08 20:52:42 +0800

    Add files via upload

---
+++ src/client/java/com/cjh/advancements/Rebirth_For_Old_Dream2_server_AdvancementsClient.java
@@ -0,0 +1,10 @@
+package com.cjh.advancements;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Rebirth_For_Old_Dream2_server_AdvancementsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/cjh/advancements/Rebirth_For_Old_Dream2_server_AdvancementsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.cjh.advancements;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Rebirth_For_Old_Dream2_server_AdvancementsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
